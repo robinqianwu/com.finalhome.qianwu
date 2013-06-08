88b39e5a4ee5de0bd290297209ebbcfffc84b9d0
Qian Wu <robinqianwu@gmail.com>
2013-06-08 17:44:20 +0200

    First Commit - all files in project added

---
+++ GitTest/src/com/finalhome/qianwu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.finalhome.qianwu.git;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git test");
+		System.out.println("made some changes");
+	}
+
+}
